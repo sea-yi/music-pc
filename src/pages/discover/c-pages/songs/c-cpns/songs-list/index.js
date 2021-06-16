c3238d867b6a0782174ccf00ad5fc5a3a3d1ed3c
seayi <552792818@qq.com>
2021-06-16 17:59:26 +0800

    update

---
+++ src/pages/discover/c-pages/songs/c-cpns/songs-list/index.js
@@ -0,0 +1,7 @@
+import React, { memo } from 'react'
+
+import { SongsListWrapper } from './style'
+
+export default memo(function SongsList() {
+  return <SongsListWrapper>SongsList</SongsListWrapper>
+})
--- src/pages/discover/c-pages/songs/index.js
+++ src/pages/discover/c-pages/songs/index.js
@@ -1,5 +1,14 @@
 import React, { memo } from 'react'
 
+import { SongsWrapper } from './style'
+import SongsHeader from './c-cpns/songs-header'
+import SongsList from './c-cpns/songs-list'
+
 export default memo(function Songs() {
-  return <div>Songs</div>
+  return (
+    <SongsWrapper className="wrap-v2">
+      <SongsHeader />
+      <SongsList />
+    </SongsWrapper>
+  )
 })
